19e3dbad0002ae91bc179017ea84cb4060d0645d
zhangpanqin <zhangpanqin@outlook.com>
2023-02-26 20:48:36 +0800

    1

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -115,20 +115,6 @@ export default {
         pageSidebarItems() {
             const page = this.$page
             const headers = groupHeaders(page.headers || [])
-            console.log(headers);
-            console.log([{
-                type: 'group',
-                collapsable: false,
-                title: page.title,
-                path: null,
-                children: headers.map(h => ({
-                    type: 'auto',
-                    title: h.title,
-                    basePath: page.path,
-                    path: page.path + '#' + h.slug,
-                    children: h.children || []
-                }))
-            }])
             return [{
                 type: 'group',
                 collapsable: false,
