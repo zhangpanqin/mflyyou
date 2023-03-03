9191453dbbe443f2c42525538012792ec1fc6349
zhangpanqin <zhangpanqin@outlook.com>
2023-03-04 02:20:10 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,30 @@
 #!/bin/bash
 set -eo pipefail
+
 BASE_DIR=$(
     cd $(dirname $0)
     pwd
 )
-# 进入项目路径下
-cd ${BASE_DIR}
 
-# 判断执行是否成功
-if [ $? != 0 ]; then
-    echo "将本地路径前缀替换错误"
-    exit -1
-fi
+dist=${BASE_DIR}/docs/.vuepress/dist
+if [ -d ${dist} ]; then
+    rm -fr ${dist}
+    echo "删除目录 ${dist}"
+else
 
-yarn run build
-hexo d
+yarn docs:build:mflyyou
+
 if [ $? != 0 ]; then
     echo "构建失败,退出"
     exit -1
 fi
 
-# 拷贝项目路径下 themes/matery/source  到 oss zhangpanqin-blog/source
+public=${BASE_DIR}/docs/.vuepress/dist
 
-/Users/panqinzhang/app/oss/ossutilmac64 cp -rf ${BASE_DIR}/themes/matery/source oss://zhangpanqin-blog/source --meta=Cache-Control:public,max-age=2592000 -c /Users/panqinzhang/.ossutilconfig
 
-if [ $? != 0 ]; then
-    echo "上传 oss 失败,退出"
-    exit -1
-fi
-
-public=${BASE_DIR}/public
-
-if [ -d ${public} ]; then
-    scp -i /Users/panqinzhang/.ssh/mflyyou/id_rsa -rp ${public}/* mflyyou@121.43.180.141:/usr/share/nginx/html/
+if [ -d ${dist} ]; then
+    scp -i /Users/panqinzhang/.ssh/mflyyou/id_rsa -rp ${dist}/* mflyyou@121.43.180.141:/usr/share/nginx/html/
     echo "部署成功"
 else
-    echo "${public} 不存在,部署失败"
+    echo "${dist} 不存在,部署失败"
 fi
